ce2061b72a2e6767eee4f2db83a7895387a74168
HoaiTam30102003 <hoaitam30102003@gmail.com>
2024-05-23 17:26:38 +0700

    Lab4

---
+++ Lab4/src/app/pages/star/star.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit {
+  @Input() rating: number = 0;
+  starWidth: number = 0;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+    this.starWidth = this.rating*90/5
+  }
+
+  onClick(): void {
+    this.ratingClicked.emit(`Đánh giá của sản phẩm là ${this.rating} sao!`);
+  }
+
+}
